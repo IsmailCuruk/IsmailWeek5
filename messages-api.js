24e967de72e9ebe0a00b1abc0fc9ffcaa800a4a1
IsmailCuruk <ismailcrk@gmail.com>
2019-05-25 15:22:09 +0200

    OK

--- messages-api.js
+++ messages-api.js
@@ -1,3 +1,4 @@
+const colors = require('colors/safe');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -8,11 +9,12 @@ app.use(bodyParser.json());
 let counter = 1
 const limitingMiddleware = (req, res, next) => {
   if (req.body.text && counter <= 5) {
-    console.log(counter, `Logging req.body.text for you ---> ${req.body.text}`)
+    console.log(counter, "Logging req.body.text for you --->")
+    console.log(colors.cyan(req.body.text))
     counter++;
   }
   else if (req.body.text && counter > 5){
-    console.warn(counter, "You have reached the limit of using the API.");
+    console.warn(colors.yellow("You have reached the limit of using the API."));
     res.status(500).end();
   }
   return next();
@@ -26,7 +28,7 @@ app.post('/messages', limitingMiddleware, (req, res, next) => {
     );
   }
   else {
-    console.error("\nERROR: The request body should have a 'text' property, but it had a '" + Object.keys(req.body) + "' property.\n")
+    console.error(colors.red("\nERROR: The request body should have a 'text' property, but it had a '" + Object.keys(req.body) + "' property.\n"))
     return res.status(400)
     .end();
   }
