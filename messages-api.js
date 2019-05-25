43afb420d1bf7b08a6eede5cac706afd75364fd1
IsmailCuruk <ismailcrk@gmail.com>
2019-05-25 15:03:32 +0200

    Assignment 1 is done and working! Added some fancy terminal logs for the teacher too .

--- messages-api.js
+++ messages-api.js
@@ -1,23 +1,36 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const port = 3000
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const validatingMiddleware = (req, res, next) => {
-  if (!req.body.text) {
-    res.status(400).json({ message: "The request body should have a 'text' property!" })
-  } else {
-    next()
+let counter = 1
+const limitingMiddleware = (req, res, next) => {
+  if (req.body.text && counter <= 5) {
+    console.log(counter, `Logging req.body.text for you ---> ${req.body.text}`)
+    counter++;
   }
-}
+  else if (req.body.text && counter > 5){
+    console.warn(counter, "You have reached the limit of using the API.");
+    res.status(500).end();
+  }
+  return next();
+};
 
-app.post('/messages', validatingMiddleware, (req, res) => {
-  console.log("\nLogging req.body.text for you...\n", req.body.text)
-  res.json({
-    text: "If you see this, you've succesfully logged req.body.text",
-  })
-})
+// app.use(limitingMiddleware)
+app.post('/messages', limitingMiddleware, (req, res, next) => {
+  if (req.body.text) {
+    res.json(
+      { message: "If you see this, you've succesfully logged req.body.text" }
+    );
+  }
+  else {
+    console.error("\nERROR: The request body should have a 'text' property, but it had a '" + Object.keys(req.body) + "' property.\n")
+    return res.status(400)
+    .end();
+  }
+  
+});
 
-app.listen(port, () => console.log(`Succes; Server is listening on port: ${port}`))
+app.listen(port, () => console.log(`Server is listening on port: ${port}\n`));
