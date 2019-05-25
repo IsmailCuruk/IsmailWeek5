eefd78aa5c1db508913123fdc4c4e6a66ab054f2
IsmailCuruk <ismailcrk@gmail.com>
2019-05-25 12:10:44 +0200

    The app responds with a status 400 when there is no req/body.text property

--- messages-api.js
+++ messages-api.js
@@ -4,10 +4,19 @@ const bodyParser = require('body-parser')
 const port = 3000
 
 app.use(bodyParser.json())
-app.post('/messages', (req, res) => {
-  console.log(req.body.text)
+
+const validatingMiddleware = (req, res, next) => {
+  if (!req.body.text) {
+    res.status(400).json({ message: "The request body should have a 'text' property!" })
+  } else {
+    next()
+  }
+}
+
+app.post('/messages', validatingMiddleware, (req, res) => {
+  console.log("\nLogging req.body.text for you...\n", req.body.text)
   res.json({
-      text: "If you see this, you've succesfully logged req.body.text",
+    text: "If you see this, you've succesfully logged req.body.text",
   })
 })
 
