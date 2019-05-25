6b72d806e5b7744e2aef51b95cc780353c6d2d69
IsmailCuruk <ismailcrk@gmail.com>
2019-05-25 11:45:12 +0200

    succesfully logs the req.body.text when you go to localhost:3000/messages

--- messages-api.js
+++ messages-api.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(bodyParser.json())
+app.post('/messages', (req, res) => {
+  console.log(req.body.text)
+  res.json({
+      text: "If you see this, you've succesfully logged req.body.text",
+  })
+})
+
 app.listen(port, () => console.log(`Succes; Server is listening on port: ${port}`))
